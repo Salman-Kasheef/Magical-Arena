8a1017d375587dc81043547f4d8786db89f6b2d7
Salman-Kasheef <salmankashif475@gmail.com>
2024-06-06 15:59:01 +0530

    Add performAttack(fight) logic to Arena class

--- src/main/java/com/game/Arena.java
+++ src/main/java/com/game/Arena.java
@@ -22,6 +22,41 @@ public class Arena {
             attacker = player2;
             defender = player1;
         }
+
+        while (player1.getHealth() > 0 && player2.getHealth() > 0){
+            performAttack(attacker, defender);
+            if (defender.getHealth() <= 0)
+                break;
+
+            //Swap attacker and defender
+            Player temp = attacker;
+            attacker = defender;
+            defender = temp;
+        }
+    }
+
+    private void performAttack(Player attacker, Player defender) {
+        int attackRoll = rollDice();
+        int defenseRoll = rollDice();
+
+        int attackDamage = attackRoll * attacker.getAttack();
+        int defense = defenseRoll * defender.getStrength();
+
+        int damage = attackDamage - defense;
+        if (defense > 0){
+            int initialHealth = defender.getHealth();
+            defender.reduceHealth(damage);
+            System.out.println(attacker.getName() + " attacks! Rolls: " + attackRoll + " (Attack damage: " + attackRoll + " * " + attacker.getAttack() + " = " + attackDamage + ")");
+            System.out.println(defender.getName() + " defends! Rolls: " + defenseRoll + "(Defense strength: " + defenseRoll + " * " + defender.getStrength() + " = " + defense + ")");
+            System.out.println(defender.getName() + "'s health reduced from " + initialHealth + " to " + defender.getHealth() + " (Damage taken: " + damage + ")");
+            System.out.println();
+        } else {
+            System.out.println(attacker.getName() + " attacks! Rolls: " + attackRoll + " (Attack damage: " + attackRoll + " * " + attacker.getAttack() + " = " + attackDamage + ")");
+            System.out.println(defender.getName() + " defends! Rolls: " + defenseRoll + " (Defense strength: " + defenseRoll + " * " + defender.getStrength() + " = " + defense + ")");
+            System.out.println(attacker.getName() + "'s attack was fully defended by " + defender.getName());
+            System.out.println();
+        }
+
     }
 
     private int rollDice(){
