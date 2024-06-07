7793834def0cf7846e2cf4b0e4ec2fc7a153b3d6
Salman-Kasheef <salmankashif475@gmail.com>
2024-06-07 12:10:07 +0530

    Add Main class to run the game

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -2,6 +2,20 @@ package com.game;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Player playerA = new Player("Player A",50,5,10);
+       Player playerB = new Player("Player B",100,10,5);
+
+        System.out.println("Starting the fight between " + playerA.getName() + " and " + playerB.getName());
+
+        Arena arena = new Arena(playerA, playerB);
+        arena.fight();
+
+        Player winner = arena.getWinner();
+        if (winner != null){
+            System.out.println("The winner is: " + winner.getName());
+        } else {
+            System.out.println("It's a draw!");
+        }
+
     }
 }
